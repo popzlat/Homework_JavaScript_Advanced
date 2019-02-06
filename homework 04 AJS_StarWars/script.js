be77e002239839affc08e7ef7632f56e85f52689
Zlatko Poposki <45499958+popzlat@users.noreply.github.com>
2019-02-07 00:39:41 +0100

    Update script.js

--- homework 04 AJS_StarWars/script.js
+++ homework 04 AJS_StarWars/script.js
@@ -1,13 +1,47 @@
+let peopleArr = [];
+let planetArr = [];
+let input = $('#inputArea');
+$(function () {
+    getPeople('https://swapi.co/api/people/');
+    getPlanets('https://swapi.co/api/planets/');
+    
+    $(document).on("click", "#table tbody tr", function () {
 
+        let name = $(this).attr("data-name");
+        let findPerson = null;
+        peopleArr.forEach(element => {
+            if (element.name === name) {
+                findPerson = element;
+            }
+        });
 
+        if (findPerson != null) {
+            
+            $('#details').html("")
+            .append(`${findPerson.name} has ${findPerson.hair_color} hair color and ${findPerson.eye_color} eyes`)
+            .css("background-color", "yellow")
+        }
+        var findPlanet = null
+        planetArr.forEach(element => {
+            if (element.name === name) {
+                findPlanet = element;
+            }
+        });
+        if (findPlanet != null) {
+            $('#details').html("")
+            .append(`${findPlanet.name} Planet is created ${findPlanet.created}`)
+            .css("background-color", "red")
+        }
+    });
+});
 
 $('#getPeople').click(function () {
+    $('#body').html("");
     $('#spin').show();
-    getPeople('https://swapi.co/api/people/');
+    appendPeople(peopleArr);
 
 });
-let peopleArr = [];
-let input = $('#inputArea');
+
 function getPeople(url) {
     fetch(url)
         .then(data => {
@@ -16,7 +50,6 @@ function getPeople(url) {
         .then((data) => {
             if (data.next) {
                 getPeople(data.next)
-
             }
 
             people(data)
@@ -30,29 +63,37 @@ function people(data) {
     let results = data.results;
     results.forEach(element => {
         peopleArr.push(element);
-        $('#body').append(`
-        <tr>
-        <td>${element.name}</td>
-        <td>${element.gender}</td>
-        <td>${element.birth_year}</td>
-        <td>${element.mass}</td>
-        <td>${element.height}</td>
-        </tr>`)
-        $('#people').show();
-        $('#planets').hide();
-        $('.logo').hide();
-        $('#spin').hide();
     });
-
 };
-let planetArr = [];
+function appendPeople(people) {
+    people.forEach(element => {
+        appendPerson(element);
+    });
+};
+
+function appendPerson(element) {
+    $('#body').append(`
+    <tr data-name="${element.name}">
+    <td>${element.name}</td>
+    <td>${element.gender}</td>
+    <td>${element.birth_year}</td>
+    <td>${element.mass}</td>
+    <td>${element.height}</td>
+    </tr>`)
+    $('#people').show();
+    $('#planets').hide();
+    $('.logo').hide();
+    $('#spin').hide();
+}
+
+
 function getPlanets(url) {
     fetch(url)
         .then(dataPlanet => {
             return dataPlanet.json();
         })
         .then((dataPlanet) => {
-            // planetArr.push(dataPlanet.results)
+
 
             if (dataPlanet.next) {
                 getPlanets(dataPlanet.next)
@@ -66,27 +107,36 @@ function getPlanets(url) {
 }
 
 $('#getPlanets').click(function () {
+    $('#body').html("");
     $('#spin').show()
-    getPlanets('https://swapi.co/api/planets/')
+    appendPlanets(planetArr);
 })
 
+function appendPlanets(planets) {
+    planets.forEach(element => {
+        apppendPlanet(element);
+
+    });
+}
+
+function apppendPlanet(element) {
+    $('#body').append(`<tr data-name="${element.name}">
+    <td>${element.name}</td>
+    <td>${element.diameter}</td>
+    <td>${element.climate}</td>
+    <td>${element.terrain}</td>
+    <td>${element.rotation_period}</td>
+    <td>${element.population}</td>
+     </tr>`);
+    $('#planets').show();
+    $('#people').hide();
+    $('.logo').hide();
+    $('#spin').hide();
+}
 function planeti(dataPlanet) {
     let results = dataPlanet.results
     results.forEach(element => {
         planetArr.push(element)
-
-        $('#body').append(`<tr> 
-            <td>${element.name}</td>
-            <td>${element.diameter}</td>
-            <td>${element.climate}</td>
-            <td>${element.terrain}</td>
-            <td>${element.rotation_period}</td>
-            <td>${element.population}</td>
-             </tr>`);
-        $('#planets').show();
-        $('#people').hide();
-        $('.logo').hide();
-        $('#spin').hide();
     });
 };
 $('#goHome').click(function () {
@@ -97,46 +147,20 @@ $('#goHome').click(function () {
     $('#spin').hide();
 });
 
-function showNames() {
-    for (const iterator of peopleArr) {
-        for (let index = 0; index < iterator.length; index++) {
-            let name = iterator[index].name;
-            console.log(name)
-        }
-    }
-}
-
-
 $("#search").on('click', function () {
     $('#body').html("");
     var searcValue = input.val();
     for (let i = 0; i < peopleArr.length; i++) {
         let element = peopleArr[i];
         if (element.name.indexOf(searcValue) >= 0) {
-            $('#body').append(`
-            <tr>
-            <td>${element.name}</td>
-            <td>${element.gender}</td>
-            <td>${element.birth_year}</td>
-            <td>${element.mass}</td>
-            <td>${element.height}</td>
-            </tr>`)
+            appendPerson(element);
         }
-        else{
-        for (let i = 0; i < planetArr.length; i++) {
-            let element = planetArr[i];
-            if (element.name.indexOf(searcValue) >= 0) {
-                $('#body').append(`<tr> 
-            <td>${element.name}</td>
-            <td>${element.diameter}</td>
-            <td>${element.climate}</td>
-            <td>${element.terrain}</td>
-            <td>${element.rotation_period}</td>
-            <td>${element.population}</td>
-             </tr>`);
+    }
+    for (let i = 0; i < planetArr.length; i++) {
+        let element = planetArr[i];
+        if (element.name.indexOf(searcValue) >= 0) {
 
-
-            }
+            apppendPlanet(element);
         }
-    }}
+    }
 });
